b648e92fbaaa65e5e500907b3255a9a1bbcff853
William Dana <william.b.dana@gmail.com>
2018-08-16 22:39:22 -0400

    Build out shell Home component; configure eslint

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import './stylesheets/App.css';
 
 import Header from './components/header/Header';
+import Home from './components/content/Home';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <p className="App-intro">
-          <code>Coming soon!</code>
-        </p>
+        <Home />
       </div>
     );
   }
