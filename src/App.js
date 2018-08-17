afb8859463e3dba6db5e889cf1000416716aef2e
William Dana <william.b.dana@gmail.com>
2018-08-16 22:41:50 -0400

    Use stateless App, Home components for time being

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import './stylesheets/App.css';
 import Header from './components/header/Header';
 import Home from './components/content/Home';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Home />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+};
 
 export default App;
--- src/components/content/Home.jsx
+++ src/components/content/Home.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import '../../stylesheets/Home.css';
 
-class Home extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <p className="App-intro">
-          <code>Coming soon!</code>
-        </p>
-      </React.Fragment>
-    );
-  }
-}
+const Home = () => {
+  return (
+    <React.Fragment>
+      <p className="App-intro">
+        <code>Coming soon!</code>
+      </p>
+    </React.Fragment>
+  );
+};
 
 export default Home;
