71f2a9c999ebe4752b69cfe611449c65e9650e21
William Dana <william.b.dana@gmail.com>
2018-08-20 18:42:11 -0400

    Lint NavBar

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import '../../stylesheets/NavBar.css';
 
-const NavBar = () => {
-  return (
-    <div className='navbar'>
-      <div className='nav-item'>
-        Home
-      </div>
-      <div className='nav-item'>
-        Forums
-      </div>
-      <div className='nav-item'>
-        Search
-      </div>
+const NavBar = () => (
+  <div className="navbar">
+    <div className="nav-item">
+      Home
     </div>
-  );
-};
+    <div className="nav-item">
+      Forums
+    </div>
+    <div className="nav-item">
+      Search
+    </div>
+  </div>
+);
 
 export default NavBar;
