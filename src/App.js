4cb6ddfe38ad4ab587ef1bba34102b0c08064233
William Dana <william.b.dana@gmail.com>
2018-08-20 18:41:04 -0400

    Add shell NavBar component; update packages; add react-router

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './stylesheets/App.css';
 
 import Header from './components/header/Header';
 import Home from './components/content/Home';
+import NavBar from './components/header/NavBar';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <NavBar />
       <Home />
     </div>
   );
